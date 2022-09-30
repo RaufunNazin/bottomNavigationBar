346a5cbacaa7755849389f761572b8294d6292e4
Raufun <raufun.nazin13@gmail.com>
2022-09-30 12:55:57 +0600

    added libraries and linked pages

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.bottomnavigationbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        loadFragment(new FirstFragment());
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    public boolean onItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.person:
+                fragment = new FirstFragment();
+                break;
+            case R.id.home:
+                fragment = new SecondFragment();
+                break;
+            case R.id.settings:
+                fragment = new ThirdFragment();
+                break;
+        }
+        if (fragment != null) {
+            loadFragment(fragment);
+        }
+        return true;
+    }
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
